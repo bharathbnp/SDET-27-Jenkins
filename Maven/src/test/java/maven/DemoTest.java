33d8556f782f0b42b900ad61516b52685a5ab7f4
Bharath Vepanjeri <Bharath Vepanjeri@Bharath-Vepanjeri>
2021-12-31 13:56:45 +0530

    Jenkins

---
+++ Maven/src/test/java/maven/DemoTest.java
@@ -0,0 +1,39 @@
+
+package maven;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{	
+	
+	@Test
+	public void demo1Test()
+	{
+	System.out.println("I am demo 1 for jenkins");
+	
+	
+	}
+	
+	
+	
+	@Test
+	public void demo2Test()
+	{
+	System.out.println("I am demo2 for jenkins");
+	}
+	
+	
+	@Test
+	public void demo3Test()
+	{
+	System.out.println("I am demo 3 for jenkins");
+	}
+	
+	@Test
+	public void demo4Test()
+	{
+	System.out.println("I am demo 4 for jenkins");
+	}
+	
+}
