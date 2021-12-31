17a4e4922ac3adf0d6232c7a6f21607bbb61c561
Bharath Vepanjeri <Bharath Vepanjeri@Bharath-Vepanjeri>
2021-12-31 13:56:45 +0530

    Jenkins

---
+++ Maven/src/test/java/maven/LoginTest.java
@@ -0,0 +1,34 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class LoginTest
+{
+
+	
+	@Test
+	public void loginTest()
+	{
+	System.out.println("I am login 1 for jenkins");
+	}
+	
+	
+	
+	@Test
+	public void login2Test()
+	{
+	System.out.println("I am login 2 for jenkins");
+	}
+	
+	
+	@Test
+	public void login3Test()
+	{
+	System.out.println("I am login 3 for jenkins");
+	}
+	
+	
+	
+	
+	
+}
